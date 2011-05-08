b0e403cdd3578755d4c35e1b7f838c95ba83a7bc
vn.hochiminh@gmail.com <vn.hochiminh@gmail.com@a43ec30a-adbf-0ae8-6705-33496c78f923>
2011-05-08 15:58:43 +0000

    Finish core source

--- trunk/Lines/gen/annam/lines/R.java
+++ trunk/Lines/gen/annam/lines/R.java
@@ -19,10 +19,20 @@ containing a value of this type.
         public static final int ItemSize=0x7f010000;
     }
     public static final class drawable {
-        public static final int green=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int red=0x7f020002;
-        public static final int yellow=0x7f020003;
+        public static final int color0=0x7f020000;
+        public static final int color1=0x7f020001;
+        public static final int color2=0x7f020002;
+        public static final int color3=0x7f020003;
+        public static final int color4=0x7f020004;
+        public static final int color5=0x7f020005;
+        public static final int color6=0x7f020006;
+        public static final int color7=0x7f020007;
+        public static final int default_icon=0x7f020008;
+        public static final int green=0x7f020009;
+        public static final int icon=0x7f02000a;
+        public static final int red=0x7f02000b;
+        public static final int yellow=0x7f02000c;
+        public static final int yellow_ball=0x7f02000d;
     }
     public static final class id {
         public static final int gridview=0x7f050002;
